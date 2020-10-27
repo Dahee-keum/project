3c7215f7d05f96e7f04ead000dea789b1d3a3ae8
Dahee-keum <70647127+Dahee-keum@users.noreply.github.com>
2020-10-27 20:57:47 +0900

    Add files via upload
    Java 리듬게임 실습

---
+++ dyanmic_beat_1/Main.java
@@ -0,0 +1,13 @@
+package dyanmic_beat_1;
+
+public class Main {
+
+		public static final int  SCREEN_WIDTH = 1280;
+		public static final int  SCREEN_WEIGHT = 720;
+	 
+	public static void main(String[] args) {
+
+			new DynamicBeat();
+	}
+
+}
